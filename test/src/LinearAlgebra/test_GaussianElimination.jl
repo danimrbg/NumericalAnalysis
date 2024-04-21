0e7a3f6778a4e1e7923891cdc645a4e88d305b4a
Daniel Moraes Bergossa <143667578+danimrbg@users.noreply.github.com>
2024-04-21 18:20:17 -0300

    Commit Bisection, FixedPoint, Newton and Secant

---
+++ test/src/LinearAlgebra/test_GaussianElimination.jl
@@ -0,0 +1,16 @@
+@testset "Test Solve System" begin
+
+    atol = 1e-10
+
+    A = [1.0 -1 1 1; 2 -1 -1 0; 1 -2 0 1; 5 0 1 -1]
+    b = [4.0, -3, 1, 5]
+    x = [0, -1, 4, -1]
+    x_calc = solve_system(A, b)
+    @test norm(x - x_calc, Inf) ≈ 0 atol = atol 
+
+    A = [1.0 -1 1 1; 2 -1 -1 0; 1 -2 0 1; 5 0 1 -1]
+    b = [4.0, -3, 1, 4]
+    x = [0, -2/3, 11/3, -1/3]
+    x_calc = solve_system(A, b)
+    @test norm(x - x_calc, Inf) ≈ 0 atol = atol 
+end
